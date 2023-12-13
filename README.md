7d435d6c6e3835792febf6d0a099b702f871d526
Yeji Han <yejicse@gmail.com>
2023-12-13 14:29:54 +0900

    pull from main

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # refinement-tutorial
-Tutorial for refinement-based verification
+Tutorial for refinement-based verification.
 
 Work In Progress.
+
+Some helpful references:
+
+A convenient technique for stuttering simulation:
+Minki Cho, Youngju Song, Dongjae Lee, Lennard Gäher, and Derek Dreyer. 2023. Stuttering for Free. Proc. ACM Program. Lang. 7, OOPSLA2, Article 281 (October 2023), 28 pages. https://doi.org/10.1145/3622857.
+
+Paco: A Coq Library for Parameterized Coinduction (https://plv.mpi-sws.org/paco/).
 
 ## Build
 Requirement: opam (>=2.0.0), Coq 8.15.0
@@ -16,10 +23,17 @@ make build -j
 
 ## Structure
 All the files are in `src/tutorial/`.
+
 `Refinement.v`
+
 -> `Imp.v`
+
 -> `FiniteSimulation.v`
+
 -> `Example1.v`
+
 -> `Example2.v`
+
 -> `Simulation.v`
--> `Example3.v`
+
+-> `Example3.v` (WIP)
--- src/tutorial/Example1.v
+++ src/tutorial/Example1.v
@@ -13,13 +13,15 @@ Set Implicit Arguments.
 Section DEMO.
   (** We will prove that src refines tgt, using the finite simulation. *)
 
-  Definition src0 : com := <{ ret 0 }>.
+  Definition src0 : com :=
+    <{ ret 0 }>.
 
-  Definition tgt0 : com := <{ "x" := (1 + 1); "y" := (2 * 1 - "x"); ret "y" }>.
+  Definition tgt0 : com :=
+    <{ "x" := (1 + 1); "y" := (2 * 1 - "x"); ret "y" }>.
 
-  Goal refines (Imp_Program1 src0) (Imp_Program1 tgt0).
+  Goal refines (Imp_Program_Mem src0) (Imp_Program_Mem tgt0).
   Proof.
-    apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src0, tgt0, Imp_init. ss.
+    apply adequacy. unfold simulation, Imp_Program_Mem, Imp_STS_Mem, src0, tgt0, Imp_init. ss.
     (* Same as 'econstructor 4.' or 'eapply sim_silentT.' *)
     econs 4.
     { ss. }
@@ -65,9 +67,9 @@ Section DEMO.
            [step_tgt_silent0 | solve_tgt_ub]
         ]).
 
-  Goal refines (Imp_Program1 src0) (Imp_Program1 tgt0).
+  Goal refines (Imp_Program_Mem src0) (Imp_Program_Mem tgt0).
   Proof.
-    apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src0, tgt0, Imp_init. ss.
+    apply adequacy. unfold simulation, Imp_Program_Mem, Imp_STS_Mem, src0, tgt0, Imp_init. ss.
     step_tgt_silent.
     step_tgt_silent.
     inv H6. inv H4. inv H5. ss.
@@ -100,7 +102,41 @@ Section DEMO.
 End DEMO.
 
 Section EX.
-  (** Prove the following refinements. Develop tactics to simplify the proofs. *)
+  (** Prove the following refinements. Develop tactics to simplify proofs. *)
+
+  (* Solves tgt undef case if tgt is not undef. *)
+  Ltac solve_tgt_ub :=
+    exfalso;
+    match goal with
+    | [UNDEF : forall _ _, ~ (ceval _ _ _) |- _] => eapply UNDEF
+    end;
+    repeat econs.
+
+  (* Makes a tgt step. *)
+  Ltac step_tgt_silent0 :=
+    match goal with
+    | [STEP: ceval _ _ _ |- _] => inv STEP
+    end;
+    ss; split; auto.
+
+  (* Combines above two tactics. *)
+  Ltac step_tgt_silent :=
+    try (econs 4;
+         [ss
+         | ss; intros ev st_tgt1 STEP0; inv STEP0;
+           [step_tgt_silent0 | solve_tgt_ub]
+        ]).
+
+  Ltac step_src_silent :=
+    try (econs 3;
+         ss; exists (inr LInternal); eexists; split;
+         [repeat econs | ss; split; auto]).
+
+  Ltac step_term :=
+    try (econs;
+         [ simpl; eauto
+           | simpl; eauto
+           | reflexivity ]).
 
   (* Solves tgt undef case if tgt is not undef. *)
   Ltac solve_tgt_ub :=
@@ -137,12 +173,13 @@ Section EX.
            | reflexivity ]).
 
   (* Ex1. Interactions with the external world is observable, so should be preserved. *)
-  Definition src1 : com := <{ "a" :=@ "print" <[0 : aexp]>; ret "a" }>.
+  Definition src1 : com :=
+    <{ "a" :=@ "print" <[0 : aexp]>; ret "a" }>.
 
   Definition tgt1 : com :=
     <{ "x" := 0; "y" :=@ "print" <["x" : aexp]>; ret "y" }>.
 
-  Goal refines (Imp_Program1 src1) (Imp_Program1 tgt1).
+  Goal refines (Imp_Program_Mem src1) (Imp_Program_Mem tgt1).
   Proof.
     apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src1, tgt1, Imp_init. ss.
     (* Make steps in tgt *)
@@ -173,13 +210,14 @@ Section EX.
     step_term.
   Qed.
 
-  (* Ex2. If semantics is given by Imp_STS1, memory accesses are also observable. *)
-  Definition src2 : com := <{ &<1> := 5; "a" := &<1>; ret "a" }>.
+  (* Ex2. If semantics is given by Imp_STS_Mem, memory accesses are also observable. *)
+  Definition src2 : com :=
+    <{ &<1> := 5; "a" := &<1>; ret "a" }>.
 
   Definition tgt2 : com :=
     <{ "x" := 2; &<1> := ("x" + 3); "y" := &<1>; ret "y" }>.
 
-  Goal refines (Imp_Program1 src2) (Imp_Program1 tgt2).
+  Goal refines (Imp_Program_Mem src2) (Imp_Program_Mem tgt2).
   Proof.
     apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src2, tgt2, Imp_init. ss.
     (* Make a step in tgt *)
@@ -214,13 +252,13 @@ Section EX.
     step_term.
   Qed.
 
-  (* But, if we want to reason about compiler optimizations for example,
-     we do not want to keep memory accesses visible.
-     Imp_STS2 is the right semantics for this.
+  (* But, if we want to reason about compiler optimizations, for example, we do not want to keep memory accesses.
+     Imp_STS_Ext is the right semantics for this. 
    *)
-  Definition src2' : com := <{ ret 5 }>.
+  Definition src2' : com :=
+    <{ ret 5 }>.
 
-  Goal refines (Imp_Program2 src2') (Imp_Program2 tgt2).
+  Goal refines (Imp_Program_Ext src2') (Imp_Program_Ext tgt2).
   Proof.
     apply adequacy. unfold simulation, Imp_Program2, Imp_STS2, src2', tgt2, Imp_init. ss.
 
@@ -238,10 +276,11 @@ Section EX.
   Qed.
 
 
-  (* Ex3. If the source program can exhibit undefined behavior, refinement always holds. *)
-  Definition src3 : com := <{ ret "a" }>.
+  (* Ex3. If the source program can exhibit UB, refinement always holds. *)
+  Definition src3 : com :=
+    <{ ret "a" }>.
 
-  Goal forall tgt, refines (Imp_Program1 src3) (Imp_Program1 tgt).
+  Goal forall tgt, refines (Imp_Program_Mem src3) (Imp_Program_Mem tgt).
   Proof.
     intros tgt.
     apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src3, Imp_init. ss.
@@ -254,7 +293,8 @@ Section EX.
   Qed.
 
   (* Ex4. If a loop always terminates, we can prove it by induction. *)
-  Definition src4 : com := <{ ret 0 }>.
+  Definition src4 : com :=
+    <{ ret 0 }>.
 
   Definition tgt4 : com :=
     <{ "x" := 100;
@@ -264,7 +304,7 @@ Section EX.
        ret "x"
       }>.
 
-  Goal refines (Imp_Program1 src4) (Imp_Program1 tgt4).
+  Goal refines (Imp_Program_Mem src4) (Imp_Program_Mem tgt4).
   Proof.
     apply adequacy. unfold simulation, Imp_Program1, Imp_STS1, src4, tgt4, Imp_init. ss.
     step_src_silent.
@@ -365,7 +405,7 @@ Section DIV.
        ret "x"
     }>.
 
-  Goal refines (Imp_Program2 src5) (Imp_Program2 tgt5).
+  Goal refines (Imp_Program_Ext src5) (Imp_Program_Ext tgt5).
   Proof.
     apply adequacy. unfold simulation, Imp_Program2, Imp_STS2, src5, tgt5, Imp_init. ss.
     do 4 step_src_silent.
@@ -425,9 +465,8 @@ Section DIV.
   Qed.
 
   (* DIV2. However, we can't prove the following refinement because it can diverge.
-     Also note that even though src5 and tgt5 are the same programs
-     (thus trivially refines each other), our simulation relation is too weak to prove this.
-     Compare this with Ex1 and Ex4.
+     Also note that even though src5 and tgt5 are the same programs (thus trivially refines each other), 
+     our simulation relation is too weak to prove this.
    *)
   Definition src6 : com :=
     <{ "x" := 100;
@@ -445,7 +484,7 @@ Section DIV.
        ret "x"
     }>.
 
-  Goal refines (Imp_Program2 src6) (Imp_Program2 tgt6).
+  Goal refines (Imp_Program_Ext src6) (Imp_Program_Ext tgt6).
   Proof.
   (* We can't prove this right now. Try to prove using induction, and see where it fails. *)
   Abort.
